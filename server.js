59d7e44b9a8c55aaf5027b3ffb2a8aa1327238bf
Paulo Seguel <pseguel@gmail.com>
2019-09-30 21:32:21 -0300

    always redirect to blackjack.html

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require("express");
- 
 var app = express();
+var targetBaseUrl = 'blackjack.html';
+
+function handleRedirect(req, res) {
+    const targetUrl = targetBaseUrl;
+    res.redirect(targetUrl);
+}
 
 app.use(express.static('public'));
 
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
+
+app.get('*', handleRedirect);
 
 var server = app.listen(8080, function(){
     var port = server.address().port;
