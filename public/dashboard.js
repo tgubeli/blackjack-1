9d90580cdfecdb454685a0641f24d4a6b9ff155e
Paulo Seguel <pseguel@gmail.com>
2019-10-10 11:56:22 -0300

    revert

--- public/dashboard.js
+++ public/dashboard.js
@@ -2,9 +2,15 @@ $(document).ready(function() {
     var guid = 'b5a8';
     var server = 'blackjack-api.apps.chile-'+guid+'.open.redhat.com';
     var api_url = 'http://'+server+'/blackjack/user/ranking';
-    this.data = '';
 
     $.getJSON(api_url, function(response) {
+        console.log(response);
+
+        // formating response
+        for (var i=0; i < response.length; i++) {
+            money = Number(response[i].balance/100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+            response[i].balance = money;
+        }
 
         $('#blackjack-ranking').dynatable({
             features: {
@@ -16,6 +22,28 @@ $(document).ready(function() {
                 records: response
             }
         });
+
+    var $chart = $('#blackjack-chart'), chart; 
+    // Set up our Highcharts chart
+    chart = new Highcharts.Chart({
+     chart: {
+      type: 'column',
+      renderTo: 'blackjack-chart'
+    },
+    title: {
+      text: 'Ranking'
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Winnings ($)'
+      }
+    },
+    series: [{
+      name: 'Users',
+      color: '#006A72'
+    }]
+  });
     });
     
 } );
