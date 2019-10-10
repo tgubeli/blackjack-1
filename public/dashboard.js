74d7d3b1367e2e64b3cac2d6457a56a5a2443148
Paulo Seguel <pseguel@gmail.com>
2019-10-10 09:35:53 -0300

    Fixed number format

--- public/dashboard.js
+++ public/dashboard.js
@@ -2,9 +2,15 @@ $(document).ready(function() {
     var guid = 'b5a8';
     var server = 'blackjack-api.apps.chile-'+guid+'.open.redhat.com';
     var api_url = 'http://'+server+'/blackjack/user/ranking';
-    this.data = '';
 
     $.getJSON(api_url, function(response) {
+        console.log(response);
+
+        // formating response
+        for (var i=0; i < response.length; i++) {
+            money = Number(response[i].balance/100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+            response[i].balance = money;
+        }
 
         $('#blackjack-ranking').dynatable({
             features: {
